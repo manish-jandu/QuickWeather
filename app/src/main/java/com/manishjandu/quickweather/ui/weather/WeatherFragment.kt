90a89f808fa4bd08f7b314cc73a21ca38c3ba1d2
Manish <manishjandu1@gmail.com>
2021-05-28 14:26:40 +0530

    ui weather fragment setup

---
+++ app/src/main/java/com/manishjandu/quickweather/ui/weather/WeatherFragment.kt
@@ -0,0 +1,25 @@
+package com.manishjandu.quickweather.ui.weather
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.manishjandu.quickweather.R
+import com.manishjandu.quickweather.databinding.FragmentWeatherBinding
+import com.zigis.segmentedarcview.custom.ArcSegment
+import com.zigis.segmentedarcview.custom.BlinkAnimationSettings
+
+class WeatherFragment : Fragment(R.layout.fragment_weather) {
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
+    }
+}
