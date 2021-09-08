1fb56ceb376855c700acfb00def988e8ca21f723
Manish <manishjandu1@gmail.com>
2021-09-08 18:23:42 +0530

    check internet live data classes added

---
+++ app/src/main/java/com/manishjandu/quickweather/utils/InternetConnectivity/ConnectionLiveData.kt
@@ -0,0 +1,85 @@
+package com.manishjandu.quickweather.utils.InternetConnectivity
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkRequest
+import android.util.Log
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+val TAG = "Connectivity Manger"
+
+class ConnectionLiveData(context: Context) : LiveData<Boolean>() {
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val validNetworks: MutableSet<Network> = HashSet()
+
+    private fun checkValidNetworks() {
+        postValue(validNetworks.size > 0)
+    }
+
+    override fun onActive() {
+        networkCallback = createNetworkCallback()
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onInactive() {
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun createNetworkCallback() = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+            val hasInternetCapability = networkCapabilities?.hasCapability(NET_CAPABILITY_INTERNET)
+            Log.d(TAG,"network is $network. and Internet capability $hasInternetCapability")
+
+            if (hasInternetCapability == true) {
+                // check if this network actually has internet
+                determineInternetAccess(network)
+            }
+        }
+        override fun onLost(network: Network) {
+            Log.d(TAG,"network removed: $network")
+            validNetworks.remove(network)
+            checkValidNetworks()
+        }
+
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            super.onCapabilitiesChanged(network, networkCapabilities)
+            if (networkCapabilities.hasCapability(NET_CAPABILITY_INTERNET)){
+                determineInternetAccess(network)
+                print("")
+            } else {
+                validNetworks.remove(network)
+                checkValidNetworks()
+            }
+        }
+
+        private fun determineInternetAccess(network: Network) {
+            CoroutineScope(Dispatchers.IO).launch {
+                val hasInternet = DoesNetworkHaveInternet.execute()
+                if(hasInternet){
+                    Log.d(TAG,"has Internet $hasInternet")
+                    withContext(Dispatchers.Main){
+                        validNetworks.add(network)
+                        checkValidNetworks()
+                    }
+                }
+            }
+        }
+    }
+
+}
